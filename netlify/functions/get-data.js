a92c465a067a8e0828bf2640e8f230319c55b22a
vipul0530 <vipul30@gmail.com>
2026-03-15 13:39:17 -0700

    Update get-data.js

--- netlify/functions/get-data.js
+++ netlify/functions/get-data.js
@@ -2,21 +2,22 @@ const { getStore } = require("@netlify/blobs");
 
 exports.handler = async (event, context) => {
   try {
-    const store = getStore("risingred");
+    const store = getStore({
+      name: "risingred",
+      siteID: process.env.NETLIFY_SITE_ID,
+      token: process.env.NETLIFY_AUTH_TOKEN
+    });
     const data = await store.get("sitedata", { type: "json" });
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify(data || {})
     };
   } catch (err) {
     console.log("get-data error:", err.message);
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({})
     };
   }
